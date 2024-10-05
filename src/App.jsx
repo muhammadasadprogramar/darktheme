d160b0c7cdbd108f3b87b9fd616b2c9151dcda90
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-10-05 13:44:07 +0500

    change title

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-    <div className="flex flex-wrap min-h-screen items-center">
+    <div className="flex px-3 flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
         <ThemeBtn></ThemeBtn>
