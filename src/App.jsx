b484b0316684e72672001d10212d1c373560f7e8
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-10-05 13:34:34 +0500

    dark theme

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Cards from "./components/Cards"
+
+const App = () => {
+
+  const [themeMode, setThemeMode] = useState("light")
+  const lightTheme = () => {
+    setThemeMode("light");
+  }
+  const darkTheme = () => {
+    setThemeMode("dark");
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark");
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
+
+
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+        <ThemeBtn></ThemeBtn>
+        <div className="w-full max-w-sm mx-auto">
+          <Cards></Cards>
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
+};
+
+export default App;
